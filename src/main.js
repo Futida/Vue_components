299cdd341f1c0628dc21ffe6ca9c026dad6dbd85
konstantingrusin <kgrushin@ncem.ru>
2017-06-15 18:40:26 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 
 
 const router = new VueRouter({
   mode: 'history',
+  base: __dirname,
   routes: [
     {
       path: '/',
