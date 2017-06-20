301b766893de963b1949fe44282e0f45a62cb9dc
konstantingrusin <kgrushin@ncem.ru>
2017-06-20 18:31:54 +0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div>
-    <nav class="navbar navbar-light" style="background-color: #e3f2fd">
-      <div class="container">
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li class="active">
-              <router-link to="/">Home</router-link>
-            </li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li><a href="#">Действие</a></li>
-                <li><a href="#">Другое действие</a></li>
-                <li><a href="#">Что-то еще</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Отдельная ссылка</a></li>
-                <li class="divider"></li>
-                <li><a href="#">Еще одна отдельная ссылка</a></li>
-              </ul>
-            </li>
-            <!--<li class="active">-->
-            <!--<router-link to="/todos">Todos</router-link>-->
-            <!--</li>-->
-            <li class="active">
-              <router-link to="/photos">Photos</router-link>
-            </li>
-          </ul>
-        </div><!-- /.navbar-collapse -->
-      </div>
-    </nav>
+    <div class="container">
+      <nav class="navbar navbar-light" style="background-color: #e3f2fd">
+        <div class="container">
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="active">
+                <router-link to="/">Home</router-link>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Действие</a></li>
+                  <li><a href="#">Другое действие</a></li>
+                  <li><a href="#">Что-то еще</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Отдельная ссылка</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Еще одна отдельная ссылка</a></li>
+                </ul>
+              </li>
+              <!--<li class="active">-->
+              <!--<router-link to="/todos">Todos</router-link>-->
+              <!--</li>-->
+              <li class="active">
+                <router-link to="/photos">Photos</router-link>
+              </li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div>
+      </nav>
+    </div>
     <router-view></router-view>
   </div><!-- /.container-fluid -->
 </template>
--- src/components/Photos.vue
+++ src/components/Photos.vue
@@ -1,36 +1,40 @@
 <template>
   <div id="app_photos">
-    <pagination :current="currentPage"
-                :total="totalPhotos"
-                :perPage="perPage"
-                @page-changed="fetchPhotos">
-    </pagination>
-    <section class="grid">
-      <div class="grid__item card" v-for='photo in photos'>
-        <div class="card__body">
-          <img :src="photo.urls.small" alt="" @click="showModal(photo)">
-        </div>
-        <div class="card__footer media">
-          <img :src="photo.user.profile_image.small" alt="" class="media__obj">
-          <div class="media__body">
-            <a href="photo.user.portfolio_url" target="_blank"> {{ photo.user.name }}</a>
+    <div class="container">
+      <pagination :current="currentPage"
+                  :total="totalPhotos"
+                  :perPage="perPage"
+                  @page-changed="fetchPhotos">
+      </pagination>
+      <div class="container-fluid">
+        <div class="grid">
+          <div class="grid__item card" v-for='photo in photos'>
+            <div class="card__body">
+              <img :src="photo.urls.small" alt="" @click="showModal(photo)">
+            </div>
+            <div class="card__footer media">
+              <img :src="photo.user.profile_image.small" alt="" class="media__obj">
+              <div class="media__body">
+                <a href="photo.user.portfolio_url" target="_blank"> {{ photo.user.name }}</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </section>
 
-    <div class="modal" v-show="flag">
-      <div class="modal-content">
-        <span class="close" @click="hideModal">X</span>
-        <img :src='path_to_largeImg' alt="" id="imgFull">
+      <pagination :current="currentPage"
+                  :total="totalPhotos"
+                  :perPage="perPage"
+                  @page-changed="fetchPhotos">
+      </pagination>
+
+      <div class="modal" v-show="flag">
+        <div class="modal-content">
+          <span class="close" @click="hideModal">X</span>
+          <img :src='path_to_largeImg' alt="" id="imgFull">
+        </div>
       </div>
     </div>
-
-    <pagination :current="currentPage"
-                :total="totalPhotos"
-                :perPage="perPage"
-                @page-changed="fetchPhotos">
-    </pagination>
   </div>
 </template>
 
@@ -64,12 +68,9 @@
 
         this.$http.get('http://api.unsplash.com/photos', options).then(function(response) {
           this.photos = response.data;
-          console.log(this.photos);
+          //console.log(this.photos);
           this.totalPhotos = parseInt(response.headers.get('x-total'));
           this.currentPage = page;
-          this.photos.forEach(function(el) {
-            el.flag = false
-          })
         }, console.log)
       },
 
