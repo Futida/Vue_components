a27d81e9cc8ef07d71ee769e291aaa2ce1dfa94b
konstantingrusin <kgrushin@ncem.ru>
2017-07-06 15:10:12 +0300

    delete folder

--- src/App.vue
+++ src/App.vue
@@ -63,9 +63,19 @@
 
 
 <style>
+
   /*импорт стилей для корректного отображения карты*/
 
   @import "../node_modules/leaflet/dist/leaflet.css";
+
+  .leaflet-fake-icon-image-2x {
+    background-image: url(../node_modules/leaflet/dist/images/marker-icon-2x.png);
+  }
+  .leaflet-fake-icon-shadow {
+    background-image: url(../node_modules/leaflet/dist/images/marker-shadow.png);
+  }
+
+
 
 
   #home:hover .popupContainer {
